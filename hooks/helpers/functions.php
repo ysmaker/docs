a01a1b244c0283e49637f7bda28b0d149e0308fb
Mihail Dzantiev <axel0726@gmail.com>
2018-04-26 11:47:57 +0300

    new version .12

--- hooks/helpers/functions.php
+++ hooks/helpers/functions.php
@@ -1,11 +1,29 @@
 <?php
 function getSSLPage($url)
 {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSLVERSION,3);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
+	 $options = [
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_HEADER         => false,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_ENCODING       => "",
+		CURLOPT_USERAGENT      => "spider",
+		CURLOPT_AUTOREFERER    => true,
+		CURLOPT_CONNECTTIMEOUT => 120,
+		CURLOPT_TIMEOUT        => 120,
+		CURLOPT_MAXREDIRS      => 10,
+		CURLOPT_SSL_VERIFYPEER => false
+	];
+
+	$ch      = curl_init( $url );
+	curl_setopt_array( $ch, $options );
+	$content = curl_exec( $ch );
+	$err     = curl_errno( $ch );
+	$errmsg  = curl_error( $ch );
+	$header  = curl_getinfo( $ch );
+	curl_close( $ch );
+
+	$header['errno']   = $err;
+	$header['errmsg']  = $errmsg;
+	$header['content'] = $content;
+	return $header;
 }
